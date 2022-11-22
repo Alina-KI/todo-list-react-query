0d88c7b824622b6edcf066758fcf307e9c427f73
Alina-KI <kraynova.alina.2002@mail.ru>
2022-11-22 20:10:10 +0300

    added api

---
+++ src/api/api.ts
@@ -0,0 +1,11 @@
+import axios from 'axios'
+
+const API_PATH = 'https://todolistreactquery-default-rtdb.firebaseio.com/'
+
+export const api = axios.create({
+  baseURL: API_PATH
+})
+
+api.interceptors.request.use((config) => {
+  return config
+})
