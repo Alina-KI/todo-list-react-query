615a7bd0b4a0163e91648ba4f9251eaf63f39475
Alina-KI <kraynova.alina.2002@mail.ru>
2022-11-24 15:42:14 +0300

    added filesTodoModal

---
+++ src/components/filesTodoModal/filesTodoModal.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import './filesTodoModal.less'
+import { TodoItem } from '../../types/todoItem'
+import { useDeleteFile } from '../../hooks/useDeleteFile'
+
+type Props = {
+  todo: TodoItem
+  isOpen: boolean
+  setIsOpen: (isOpen: boolean) => void
+}
+
+export const FilesTodoModal = ({ todo, isOpen, setIsOpen }: Props) => {
+  const deleteFile = useDeleteFile()
+
+  return (
+    isOpen
+      ?
+      <div className="filesTodoModal">
+        <div className="formFilesTodoModal">
+          {todo.files.map(file =>
+            <button className="downloadFileButton" key={file.name}
+                    onClick={() => deleteFile({ file: file, todo: todo })}>
+              <img className="image" src={file.path} alt=""/>
+            </button>
+          )}
+          <button className='cross' onClick={() => setIsOpen(false)}>X</button>
+        </div>
+      </div>
+      :
+      null
+  )
+}
